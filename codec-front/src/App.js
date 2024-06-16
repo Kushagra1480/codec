b370a6e902aba23984f724d3f6f2e1dfba787fe8
Kushagra1480 <kushagrakartik1480@gmail.com>
2024-06-16 16:55:44 -0700

    Added Routes

--- codec-front/src/App.js
+++ codec-front/src/App.js
@@ -1,13 +1,15 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {Landing} from "./components/Landing"
+import {Room} from "./components/Room"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" />
-          <Route path="/" />
+          <Route path="/" element={<Landing />}/>
+          <Route path="/room" element={<Room />}/>
         </Routes>
       </BrowserRouter>
     </div>
