3e48b9d30387a167532377a9f2257b55d90fc1d5
Kushagra1480 <kushagrakartik1480@gmail.com>
2024-06-17 18:49:14 -0700

    Created Server

--- codec-front/src/components/Room.js
+++ codec-front/src/components/Room.js
@@ -1,7 +1,11 @@
 import { useSearchParams } from "react-router-dom"
+import { useEffect } from "react"
 
 export const Room = () => {
     const [searchParams, setSearchParams] = useSearchParams()
+    useEffect(() => {
+        
+    }, [name])
     const name = searchParams.get("name")
     return (
         <div>
